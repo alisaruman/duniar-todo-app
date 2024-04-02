3923d13a3d016858df1fb61d24f4787c5c69e68b
alisaruman <ali_heidary007@yahoo.com>
2024-04-02 20:12:05 +0330

    Feature -> install react.js + dependencies, static Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-primary p-5">
+      <div
+        id="breadcrumb"
+        className="flex items-center gap-2 text-white font-semibold text-xl"
+      >
+        <h1 className="pb-1">Task Management</h1>
+        <i className="icon-chevron-right w-3 h-3"></i>
+        <span className="pb-1">Home</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
